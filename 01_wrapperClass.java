46f41c4837d4c225bc0fdaad01e5637581382f0e
Om Kaushik <omkaushik015@gmail.com>
2023-02-12 20:29:47 +0530

    complete wrapper class

--- 01_wrapperClass.java
+++ 01_wrapperClass.java
@@ -1,15 +1,18 @@
 class Main{
 
 public static void main(String args[]){
+	// primitive data type
 	int a = 10;
 	double b = 19.45;
 
 	System.out.println("The value of a: "+a);
 	System.out.println("The value of b: "+b);
 
+	// wrapper class
 	Integer x = a;
 	Double y = b;
 
+	// This automatic conversion to primitive data type to object is known as Autoboxing.
 	if(x instanceof Integer){
 	 System.out.println("x belongs to Integer class");
 	 System.out.println("X.intValue() :"+x.intValue());
@@ -18,6 +21,15 @@ public static void main(String args[]){
 	 System.out.println("y belongs to Double class");
 	 System.out.println("y.doubleValue() : "+y.doubleValue());
 	}
-    }
+
+	// Converting object type to primitive data type
+	int i = x;
+	double j = y;
+
+	// This process is called unboxing.
+	
+	System.out.println("i : "+i);
+	System.out.println("j : "+j);
+}
 
 }
